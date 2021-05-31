e63d2c54f6e855e40a6e3f40be503b641cb417a7
unknown <nicolas.baudvin@gmail.com>
2021-05-31 11:13:12 +0200

    Footer & Desc Component

---
+++ Components/Footer/index.js
@@ -0,0 +1,42 @@
+import Link from 'next/link';
+import { AiFillFacebook, AiOutlineTwitter, AiOutlineInstagram } from 'react-icons/ai';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="footer-block">
+                <img src="/img/shared/desktop/logo.svg" alt="audiophile" />
+                <p className="footer-block-text">
+                    Audiophile is an all in one stop to fulfill your audio needs. We're a small team
+                    of music lovers and sound specialists who are devoted to helping you get the
+                    most out of personal audio. Come and visit our demo facility - we’re open 7 days
+                    a week.
+                </p>
+                <p className="footer-block-text">Copyright 2021. All Rights Reserved</p>
+            </div>
+            <div className="footer-block">
+                <nav>
+                    <Link href="/" as="a">
+                        Home
+                    </Link>
+                    <Link href="/" as="a">
+                        Headphones
+                    </Link>
+                    <Link href="/" as="a">
+                        Speakers
+                    </Link>
+                    <Link href="/" as="a">
+                        Earphones
+                    </Link>
+                </nav>
+                <div className="footer-block-social">
+                    <AiFillFacebook />
+                    <AiOutlineTwitter />
+                    <AiOutlineInstagram />
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- Components/Home/Desc/index.js
+++ Components/Home/Desc/index.js
@@ -1,5 +1,22 @@
 const Desc = () => {
-    return <div className="desc"></div>;
+    return (
+        <div className="desc">
+            <div className="desc-block">
+                <h1 className="desc-block-title">
+                    Bringing you the <span>best</span> audio gear
+                </h1>
+                <p className="desc-block-text">
+                    Located at the heart of New York City, Audiophile is the premier store for high
+                    end headphones, earphones, speakers, and audio accessories. We have a large
+                    showroom and luxury demonstration rooms available for you to browse and
+                    experience a wide range of our products. Stop by our store to meet some of the
+                    fantastic people who make Audiophile the best place to buy your portable audio
+                    equipment.
+                </p>
+            </div>
+            <img src="/img/shared/desktop/image-best-gear.jpg" alt="best gear" />
+        </div>
+    );
 };
 
 export default Desc;
