99223ad3852ac8a564aee8f1e9fadca9feee5a1b
unknown <nicolas.baudvin@gmail.com>
2021-05-31 11:13:34 +0200

    Footer & Desc Styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,10 @@
-import '../Styles/Home/home.scss';
 import '../Styles/header.scss';
 import '../Styles/global.scss';
 import '../Styles/vars.scss';
+import '../Styles/footer.scss';
+import '../Styles/Home/home.scss';
 import '../Styles/Home/someproducts.scss';
+import '../Styles/Home/desc.scss';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />;
