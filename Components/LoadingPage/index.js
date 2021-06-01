1896911ce8aa85cbfa3c62ebca910fa13b2a8c2b
unknown <nicolas.baudvin@gmail.com>
2021-06-01 17:35:22 +0200

    loading page

---
+++ Components/LoadingPage/index.js
@@ -0,0 +1,9 @@
+const LoadingPage = () => {
+    return (
+        <div className="loading">
+            <div className="bounceball"></div>
+            <div className="text">Loading ...</div>
+        </div>
+    );
+};
+export default LoadingPage;
