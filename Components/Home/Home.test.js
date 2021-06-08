200e891f27b1b9983c903aa1ca673a81e28996bb
unknown <nicolas.baudvin@gmail.com>
2021-06-08 20:38:39 +0200

    home tests

--- Components/Home/Home.test.js
+++ Components/Home/Home.test.js
@@ -1,9 +1,26 @@
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import HomePage from ".";
+import Link from 'next/link';
+
+jest.mock('next/link', () => ({ children }) => children);
+
+const linksData = [
+    '/headphones',
+    '/speakers',
+    '/earphones'
+];
 
 describe("<HomePage />", () => {
     it("should render", () => {
         const wrapper = shallow(<HomePage />);
         expect(wrapper).toBeTruthy();
+    });
+
+    it("should have the correct href", () => {
+        const wrapper = mount(<HomePage />);
+        const links = wrapper.find('.categories').find(Link);
+        links.forEach((link, i) => {
+            expect(link.props().href).toEqual(linksData[i]);
+        })
     });
 });
