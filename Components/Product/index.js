2d1214a193036e321c95ddbf8172dd4a7b49a05f
unknown <nicolas.baudvin@gmail.com>
2021-06-08 20:39:25 +0200

    wip: product

---
+++ Components/Product/index.js
@@ -0,0 +1,69 @@
+/* eslint-disable react/no-unescaped-entities */
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+
+const Product = ({ state }) => {
+    const [quantity, setQuantity] = useState(1);
+    const { product } = state;
+    return (
+        <div className="product">
+            {!product && (
+                <p className="product-avert">
+                    It seems that you have requested a product that doesn't exist anymore
+                </p>
+            )}
+            {product && (
+                <div className="product-container">
+                    <div className="product-block">
+                        <div className="product-block--horizontal">
+                            <img src={product.image} alt={product.name} />
+                        </div>
+                        <div className="product-block--horizontal">
+                            {product.isNewProduct && (
+                                <h3 className="product-item-isnew">new product</h3>
+                            )}
+                            <h1 className="product-item-title">{product.name}</h1>
+                            <p className="product-item-desc">{product.desc}</p>
+                            <strong className="product-item-price">$ {product.price}</strong>
+                            <div className="product-item-purshase">
+                                <div className="product-item-quantity">
+                                    <button
+                                        onClick={() =>
+                                            setQuantity(quantity - 1 === 0 ? 1 : quantity - 1)
+                                        }>
+                                        -
+                                    </button>
+                                    <b> {quantity} </b>
+                                    <button onClick={() => setQuantity(quantity + 1)}>+</button>
+                                </div>
+                                <button className="product-item-add">add to cart</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="product-block">
+                        <div className="product-block--horizontal"></div>
+                        <div className="product-block--horizontal"></div>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+Product.propTypes = {
+    state: PropTypes.shape({
+        product: PropTypes.oneOfType([
+            PropTypes.bool,
+            PropTypes.shape({
+                desc: PropTypes.string.isRequired,
+                name: PropTypes.string.isRequired,
+                image: PropTypes.string.isRequired,
+                isNewProduct: PropTypes.bool.isRequired,
+                price: PropTypes.number.isRequired,
+                _id: PropTypes.string.isRequired
+            })
+        ])
+    })
+};
+
+export default Product;
