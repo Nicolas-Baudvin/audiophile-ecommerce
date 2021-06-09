a9993c9e30df8829799e519579d783a44344b710
unknown <nicolas.baudvin@gmail.com>
2021-06-09 12:18:09 +0200

    add features & in the box items

--- model/Headphones.js
+++ model/Headphones.js
@@ -5,7 +5,9 @@ const HeadphonesSchema = new mongoose.Schema({
     name: { type: String, required: true },
     desc: { type: String, required: true },
     image: { type: String, required: true },
-    price: { type: String, required: true }
+    price: { type: String, required: true },
+    features: { type: String, required: true },
+    inTheBox: { type: Array, required: true }
 });
 
 export default mongoose.models.Headphones || mongoose.model('Headphones', HeadphonesSchema);
--- model/Speakers.js
+++ model/Speakers.js
@@ -5,7 +5,9 @@ const SpeakersSchema = new mongoose.Schema({
     name: { type: String, required: true },
     desc: { type: String, required: true },
     image: { type: String, required: true },
-    price: { type: String, required: true }
+    price: { type: String, required: true },
+    features: { type: String, required: true },
+    inTheBox: { type: Array, required: true }
 });
 
 export default mongoose.models.Speakers || mongoose.model('Speakers', SpeakersSchema);
